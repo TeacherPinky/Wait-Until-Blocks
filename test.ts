371a5f01bb1404aca9ebddafb83a61a4bb1d9acb
TeacherPinky <91179976+TeacherPinky@users.noreply.github.com>
2021-09-22 14:28:18 +0200

    moved the code from main.ts to test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,8 @@
-// tests gaan hier; deze zal niet worden gecompileerd wanneer dit pakket wordt gebruikt als extensie.
+WaitUntilBlocks.waitUntilButtonReleased(Button.A)
+WaitUntilBlocks.waitUntilButtonPressed(Button.A)
+WaitUntilBlocks.waitUntilSoundLevelBelow(80)
+WaitUntilBlocks.waitUntilPinReleased(TouchPin.P0)
+WaitUntilBlocks.waitUntilPinPressed(TouchPin.P0)
+basic.forever(function () {
+
+})
