8ade39197cb3b52c7b95a99f7988dfa6bfd527b2
TeacherPinky <91179976+TeacherPinky@users.noreply.github.com>
2021-09-25 19:45:26 +0200

    added Swedish translation of the blocks

--- waitUntil.ts
+++ waitUntil.ts
@@ -10,6 +10,7 @@ namespace WaitUntilBlocks {
     //% blockId=WaitUntilBlocks_wait_until_pin_pressed
     //% block="wait until pin %pin is pressed"
     //% block.loc.de="warte bis Pin %pin gedrückt"
+    //% block.loc.sv-SE="vänta tills pin %pin trycks"
     //% block.loc.fr="attendre jusqu'à ce que la broche %pin est presée"
     //% block.loc.nl="wacht tot pin %pin is ingedrukt"
     //% jsdoc.loc.nl="Wacht tot een pin is ingedrukt"
@@ -27,6 +28,7 @@ namespace WaitUntilBlocks {
     //% blockId=WaitUntilBlocks_wait_until_not_pin_pressed
     //% block="wait until pin %pin is released"
     //% block.loc.de="warte bis Pin %pin losgelassen"
+    //% block.loc.sv-SE="vänta tills pin %pin släpps"
     //% block.loc.fr="attendre jusqu'à ce que la broche %pin est relachée"
     //% block.loc.nl="wacht tot pin %pin is losgelaten"
     //% jsdoc.loc.nl="Wacht tot een pin is losgelaten, oftewel niet meer wordt ingedrukt."
@@ -44,6 +46,7 @@ namespace WaitUntilBlocks {
     //% blockId=WaitUntilBlocks_wait_until_sound_level_below
     //% block="wait until sound level is below %level"
     //% block.loc.de="warte bis Lautstärke ist unter %level"
+    //% block.loc.sv-SE="vänta tills ljudnivån är under %level"
     //% block.loc.fr="attendre jusqu'à ce que le niveau sonore est de moins de %level"
     //% block.loc.nl="wacht tot geluidsniveau lager is dan %level"
     //% jsdoc.loc.nl="Wacht tot het geluidsniveau onder de gekozen waarde komt"
@@ -62,6 +65,7 @@ namespace WaitUntilBlocks {
     //% blockId=Wachtblokken_wait_until_sound_level_above
     //% block="wait until sound level is above %level"
     //% block.loc.de="warte bis Lautstärke ist oben %level"
+    //% block.loc.sv-SE="vänta tills ljudnivån är över %level"
     //% block.loc.fr="attendre jusqu'à ce que le niveau sonore au-dessus %level"
     //% block.loc.nl="wacht tot geluidsniveau hoger is dan %level"
     //% jsdoc.loc.nl="Wacht tot het geluidsniveau boven de gekozen waarde komt"
@@ -80,6 +84,7 @@ namespace WaitUntilBlocks {
     //% blockId=WaitUntilBlocks_wait_until_button_is_pressed
     //% block="wait until button %button is pressed"
     //% block.loc.de="warte bis Knopf %button gedrückt"
+    //% block.loc.sv-SE="vänta tills knapp %button trycks"
     //% block.loc.fr="attendre jusqu'à ce que le bouton %button est pressé"
     //% block.loc.nl="wacht tot knop %button is ingedrukt"
     //% jsdoc.loc.nl="Wacht tot een knop is ingedrukt."
@@ -97,6 +102,7 @@ namespace WaitUntilBlocks {
     //% blockId=WaitUntilBlocks_wait_until_button_is_released
     //% block="wait until button %button is released"
     //% block.loc.de="warte bis Knopf %button losgelassen"
+    //% block.loc.sv-SE="vänta tills knapp %button släpps"
     //% block.loc.fr="attendre jusqu'à ce que le bouton %button est relaché"
     //% block.loc.nl="wacht tot knop %button is losgelaten"
     //% jsdoc.loc.nl="Wacht tot een knop is losgelaten oftewel niet meer is ingedrukt."
