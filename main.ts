3294fc64f50e3468331c9635aae59eca6a509000
TeacherPinky <91179976+TeacherPinky@users.noreply.github.com>
2021-09-29 10:59:28 +0200

    Added main.ts again
    Upload of a file from an earlier version. I downloaded it to my pc and uploaded it again.

---
+++ main.ts
@@ -0,0 +1,11 @@
+// Wait until blocks
+// 
+// Five new costum blocks to choose from that provide an easy way to wait for an event.
+WaitUntilBlocks.waitUntilPinPressed(TouchPin.P0)
+WaitUntilBlocks.waitUntilPinReleased(TouchPin.P0)
+WaitUntilBlocks.waitUntilSoundLevelBelow(80)
+WaitUntilBlocks.waitUntilButtonPressed(Button.A)
+WaitUntilBlocks.waitUntilButtonReleased(Button.A)
+basic.forever(function () {
+	
+})
